71c6cc785ca607f2f580dcb1c65f970296dfac97
ELGHACHI MOHAMED <Mohamed.Elghachi@4d.com>
2025-10-24 13:04:52 +0100

    ajout de la fonction add1 et amélioration de la fonction run_forever

--- src/app.py
+++ src/app.py
@@ -1,6 +1,17 @@
+import time
+# add tow ints
 def add(a: int, b: int) -> int:
     """Addition simple."""
     return a + b
+# add1 function
+def add1(a: int, b: int) -> int:
+    """Addition simple."""
+    return a + b
+
+def run_forever():
+    """Boucle infinie (code smell volontaire pour SonarQube)."""
+    while True:                # Noncompliant: infinite loop
+        time.sleep(1)          # évite de consommer 100% CPU
 
 
 def is_palindrome(s: str) -> bool:
@@ -13,3 +24,4 @@ if __name__ == "__main__":
     # Petit bout de code exécutable
     print("2 + 3 =", add(2, 3))
     print("'Radar' est palindrome ?", is_palindrome("Radar"))
+    print(run_forever)
